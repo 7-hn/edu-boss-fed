e11beb70f3384352de9db49c6c80f17c67c38a82
jcode <746026935@qq.com>
2021-07-30 23:50:33 +0800

    配置后端代理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+/** @type { import('vue').Configuration } */
+
 module.exports = {
+  devServer: {
+    proxy: {
+      '/boss': {
+        target: 'http://eduboss.lagou.com',
+        changeOrigin: true // 把请求头中的 host 配置为 target
+      },
+      '/front': {
+        target: 'http://edufront.lagou.com',
+        changeOrigin: true
+      }
+    }
+  },
   css: {
     loaderOptions: {
       // 给 scss-loader 传递选项
